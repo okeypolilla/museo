dc134ac9e90220cf7d1d98932a33106ec0c63ccc
okeypolilla <89881186+okeypolilla@users.noreply.github.com>
2022-10-20 13:44:44 -0300

    FIX:BD

--- database/database.js
+++ database/database.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql')
-const {database}= require('./keys')
-const pool = mysql.createConnection({database});
 
-pool.connect((err, connection)=>{
-  if(err){
-      if (err.code=== 'PROTOCOL_CONNECTION_LOST'){
-          console.error('Database lost')
-      }
-      if(err.code === 'ECONNREFUSED'){
-          console.error('Database connection was refused')
-      }
-  }
-      if(connection) connection.release()
-      console.log('DB is connected')
-        return;
-  })
-
+var connection = mysql.createConnection({
+   host: 'sql10.freemysqlhosting.net',
+   user: 'sql10528009',
+   password: 'P2mqgKPswt',
+   database: 'sql10528009',
+   port: 3306
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta.');
+   }
+});
+//connection.end();  //para finalizar la coneccion de la base de datos
  
+const pool = connection;
 module.exports = pool;
