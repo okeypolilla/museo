1ddb8cf210ab7c37adb4b1040ee6ceaf2ad2d66d
okeypolilla <89881186+okeypolilla@users.noreply.github.com>
2022-10-24 19:42:24 -0300

    sacando_express

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const express = require('express'); //
-const app = express();
-const morgan = require('morgan'); //
+const http= require('http');
+const fs = require('fs')
+const { isBuffer } = require('util');
 
-const pool = require('./database/database.js');
+const server = http.createServer((req,res) => {
+  const {url,method} = req;
 
-app.set('port', 8888);
+  //logger
+  console.log('URL: ${url} - Method: ${method}');
+  fs.readFileSync(__dirname + '/src/public',function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data)
+    res.end();
+  });
+
+
+})
+
+server.listen(8888);
+//const express = require('express'); //
+//const app = express();
+//const morgan = require('morgan'); //
+
+//const pool = require('./database/database.js');
+
+//app.set('port', 8888);
 
 
 //middlewares
-app.use(morgan('dev')); 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+//app.use(morgan('dev')); 
+//app.use(express.json());
+//app.use(express.urlencoded({extended: false}));
 
 //routes 
 //app.use('view engine','ejs');
 //app.use(require("./rutas/index.js"));
   //Usuario
-app.use(require('./controladores/users.js'));
-app.use(express.static(__dirname + '/src/public'))
+//app.use(require('./controladores/users.js'));
+//app.use(express.static(__dirname + '/src/public'))
 
 
 
 //iniciar 
-app.listen(app.get('port'),()=>{  
- console.log('Listening on port ',app.get('port'));
-});
+//app.listen(app.get('port'),()=>{  
+ //console.log('Listening on port ',app.get('port'));
+//});
