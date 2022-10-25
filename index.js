e252c8f3ea467074fa9d6942106a5be45835768c
okeypolilla <89881186+okeypolilla@users.noreply.github.com>
2022-10-25 12:02:22 -0300

    urlvercel1

--- index.js
+++ index.js
@@ -1,16 +1,82 @@
 
-const http= require('http');
-const fs = require('fs');
-const pool = require('./database/database');
+// const http= require('http');
+// const fs = require('fs');
+// const pool = require('./database/database');
 
-const mime = {
-  'html': 'text/html',
-  'css': 'text/css',
-  'jpg': 'image/jpg',
-  'ico': 'image/x-icon',
-  'mp3': 'audio/mpeg3',
-  'mp4': 'video/mp4'
-}
+// const mime = {
+//   'html': 'text/html',
+//   'css': 'text/css',
+//   'jpg': 'image/jpg',
+//   'ico': 'image/x-icon',
+//   'mp3': 'audio/mpeg3',
+//   'mp4': 'video/mp4'
+// }
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './src/index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8888);
+console.log('Server running at http://127.0.0.1:8125/');
+
+
+
+
+
+
+
+
+
+
+
 
 // const servidor = http.createServer((req, res) => {
 //   const url = new URL('http://localhost:8888' + req.url)
@@ -47,24 +113,24 @@ const mime = {
 
 // console.log('Servidor web iniciado')
 
-const server = http.createServer((req,res) => {
-  const url = new URL('https://museo-tau.vercel.app/' + req.url)
-  let camino = 'src' + url.pathname
-  if (camino == 'src/')
-    camino = './src/index.html'
-    const vec = camino.split('.')
-   const extension = vec[vec.length - 1]
-    const mimearchivo = mime[extension]
-  console.log(url + ':url')
-  fs.readFile(camino,function(err, data){
-      res.writeHead(200, {'Content-Type': mimearchivo});
-      res.write(data)
-    res.end();
+// const server = http.createServer((req,res) => {
+//   const url = new URL('http://localhost:8888' + req.url)
+//   let camino = 'src' + url.pathname
+//   if (camino == 'src/')
+//     camino = './src/index.html'
+//     const vec = camino.split('.')
+//    const extension = vec[vec.length - 1]
+//     const mimearchivo = mime[extension]
+//   console.log(url + ':url')
+//   fs.readFile(camino,function(err, data){
+//       res.writeHead(200, {'Content-Type': mimearchivo});
+//       res.write(data)
+//     res.end();
 
-  })
-});
-   server.listen(8888);
-   console.log('Servidor web iniciado')
+//   })
+// });
+//    server.listen(8888);
+//    console.log('Servidor web iniciado')
 
 
 
